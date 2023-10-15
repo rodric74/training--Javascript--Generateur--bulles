9548b92cd13947ab19c187ee80db3c7cd2ddc834
rodric74 <rodric.rambosson@gmail.com>
2023-10-15 20:23:26 +0200

    bubble animation

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const bubble = document.createElement("span");
 bubble.classList.add("bubble");
 document.body.appendChild(bubble);
 
-console.log(Math.random());
+// console.log(Math.random() * 200 + 100);
+const size = Math.random() * 200 + 100 + "px";
+// console.log(size);
+bubble.style.height = size;
+bubble.style.width = size;
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
