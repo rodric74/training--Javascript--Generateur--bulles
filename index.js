73a46538f2d761ad5ca44b67713dccc227945bbb
rodric74 <rodric.rambosson@gmail.com>
2023-10-15 20:29:02 +0200

    bubble multiplication

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const bubble = document.createElement("span");
-// console.log(bubble);
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  // console.log(bubble);
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-// console.log(Math.random() * 200 + 100);
-const size = Math.random() * 200 + 100 + "px";
-// console.log(size);
-bubble.style.height = size;
-bubble.style.width = size;
+  // console.log(Math.random() * 200 + 100);
+  const size = Math.random() * 200 + 100 + "px";
+  // console.log(size);
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-bubble.style.setProperty("--left", Math.random() * 100 + "%");
+  bubble.style.setProperty("--left", Math.random() * 100 + "%");
+};
+
+setInterval(bubbleMaker, 3000);
